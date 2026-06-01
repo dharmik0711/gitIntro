dd81f053892254255d8d7a3ead6f071f7f1d33ab
Dharmik Patel <patel93@sheridancollege.ca>
2026-06-01 12:33:14 -0400

    First commit

---
+++ gitIntro/src/main/java/inclassexercise/gitintro/GitIntro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package inclassexercise.gitintro;
+
+/**
+ *
+ * @author Dharmik Patel,2026
+ */
+public class GitIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
